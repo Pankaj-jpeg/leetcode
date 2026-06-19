8559f5d9239d94864742f0dd5e459bcd92ba7312
Pankaj Sharma <pankajs8727@gmail.com>
2026-06-19 16:32:14 +0530

    solve(easy): Remove Element [cpp] time=N/A

--- easy/0027-remove-element.cpp
+++ easy/0027-remove-element.cpp
@@ -3,7 +3,7 @@
 // Difficulty : Easy
 // Link       : https://leetcode.com/problems/remove-element/
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// Approach: two-pointer technique, one for iterating through the array and one for keeping track of the position to place non-target elements | Time: O(n) | Space: O(1)
+// Approach: two-pointer technique, one for iterating and one for writing | Time: O(n) | Space: O(1)
 // Time       : 
 // Space      : 
 // Runtime    : 0 ms  |  Memory: 11.7 MB
