747c224f3bfd3fa86f27d789679435eb02242703
Pankaj Sharma <pankajs8727@gmail.com>
2026-06-19 16:23:46 +0530

    solve(easy): Check Good Integer [cpp] time=N/A

---
+++ easy/3959-check-good-integer.cpp
@@ -0,0 +1,30 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// 3959. Check Good Integer
+// Difficulty : Easy
+// Link       : https://leetcode.com/problems/check-good-integer/
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Approach: single pass through the digits of the input number | Time: O(log(n)) | Space: O(1)
+// Time       : 
+// Space      : 
+// Runtime    : 0 ms  |  Memory: 7.7 MB
+// Date       : 2026-06-14
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+class Solution {
+public:
+    bool checkGoodInteger(int n) {
+        int sum =0 ;int ssum=0;int r;
+        if(n==0){
+            return false;
+        }
+        while(n>0){
+            r = n%10;
+            n = n/10;
+            sum+=r;
+            ssum+=r*r;
+        }
+
+        return ssum - sum >= 50;
+        
+    }
+};
