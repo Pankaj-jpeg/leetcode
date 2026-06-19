db8ca20f153e84153644e3ebb5754421dfa94754
Pankaj Sharma <pankajs8727@gmail.com>
2026-06-19 16:25:51 +0530

    solve(easy): Merge Two Sorted Lists [cpp] time=N/A

---
+++ easy/0021-merge-two-sorted-lists.cpp
@@ -0,0 +1,69 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// 21. Merge Two Sorted Lists
+// Difficulty : Easy
+// Link       : https://leetcode.com/problems/merge-two-sorted-lists/
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// 
+// Time       : 
+// Space      : 
+// Runtime    : 0 ms  |  Memory: 19.5 MB
+// Date       : 2026-01-10
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode* head=nullptr;
+        ListNode* prev=nullptr;
+        ListNode* current=nullptr;
+
+        while(list1!=nullptr && list2!=nullptr){
+
+            if(list1->val <= list2->val ){
+                  current = list1;
+                  list1=list1->next; 
+            }
+            else{
+                current = list2;
+                list2=list2->next;
+            }
+
+            if(head==nullptr){
+                head = current;
+                prev = current;
+            }
+            else{
+                prev->next = current;
+                prev = current;
+            }
+    
+    }
+    if(list1!=nullptr){
+        if(head==nullptr){
+            head = list1;
+        }
+        else{
+                prev->next = list1;
+            }
+    }
+    if(list2!=nullptr){
+        if(head==nullptr){
+            head = list2;
+        }
+        else{
+                prev->next = list2;
+            }
+    }
+    return head;
+}
+};
