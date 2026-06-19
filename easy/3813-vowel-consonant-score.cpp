41eec364a4596b89d3cade34921baf340d338efb
Pankaj Sharma <pankajs8727@gmail.com>
2026-06-19 16:25:42 +0530

    solve(easy): Vowel-Consonant Score [cpp] time=N/A

---
+++ easy/3813-vowel-consonant-score.cpp
@@ -0,0 +1,32 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// 3813. Vowel-Consonant Score
+// Difficulty : Easy
+// Link       : https://leetcode.com/problems/vowel-consonant-score/
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Approach: simple iteration | Time: O(n) | Space: O(1))
+// Time       : 
+// Space      : 
+// Runtime    : 1 ms  |  Memory: 9.1 MB
+// Date       : 2026-01-18
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+class Solution {
+public:
+    int vowelConsonantScore(string s) {
+        int i=0;int v=0;int c=0;
+        while(s[i]!='\0'){
+            if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u'){
+                v++;
+            }
+            if(s[i]=='b' || s[i]=='c' || s[i]=='d' || s[i]=='f' || s[i]=='g' || s[i]=='h' || s[i]=='j' || s[i]=='k' || s[i]=='l' || s[i]=='m' || s[i]=='n' || s[i]=='p' || s[i]=='q' || s[i]=='r' || s[i]=='s' || s[i]=='t' || s[i]=='v' || s[i]=='w' || s[i]=='x' || s[i]=='y' || s[i]=='z'){
+                c++;
+            }
+            i++;
+        }
+
+        if(c>0)
+            return v/c;
+        else
+            return 0;
+    }
+};
