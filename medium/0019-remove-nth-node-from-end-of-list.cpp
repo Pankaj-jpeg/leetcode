eb0bac76e9af01458b1a4ae48556ef8c85560ce7
Pankaj Sharma <pankajs8727@gmail.com>
2026-06-19 16:23:23 +0530

    solve(medium): Remove Nth Node From End of List [cpp] time=N/A

---
+++ medium/0019-remove-nth-node-from-end-of-list.cpp
@@ -0,0 +1,49 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// 19. Remove Nth Node From End of List
+// Difficulty : Medium
+// Link       : https://leetcode.com/problems/remove-nth-node-from-end-of-list/
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Approach: two-pointer technique with a slow and fast pointer | Time: O(L) | Space: O(1)
+// Time       : 
+// Space      : 
+// Runtime    : 0 ms  |  Memory: 15 MB
+// Date       : 2026-06-15
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+        
+        if(head == NULL || head->next == NULL){
+            return NULL;
+        }
+
+        for(int i=0;i<n;i++){
+            fast = fast->next;
+        }
+        if(!fast){
+            return head->next;
+        }
+        while(fast->next!=NULL){
+            slow = slow->next;
+            fast = fast->next;
+        }
+
+        slow->next = slow->next->next;
+
+        return head;
+
+    }
+};
