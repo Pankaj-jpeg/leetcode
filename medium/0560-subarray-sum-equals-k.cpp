d846be120b9f8e216305ee9880b29d7762a16f50
Pankaj Sharma <pankajs8727@gmail.com>
2026-06-19 16:25:00 +0530

    solve(medium): Subarray Sum Equals K [cpp] time=N/A

---
+++ medium/0560-subarray-sum-equals-k.cpp
@@ -0,0 +1,34 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// 560. Subarray Sum Equals K
+// Difficulty : Medium
+// Link       : https://leetcode.com/problems/subarray-sum-equals-k/
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Approach: prefix sum with hashmap | Time: O(n) | Space: O(n)
+// Time       : 
+// Space      : 
+// Runtime    : 32 ms  |  Memory: 45.4 MB
+// Date       : 2026-05-26
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        unordered_map <int,int> map;int sum=0;int cnt=0;
+        for(int i=0;i<nums.size();i++){
+            sum+=nums[i];
+            if(sum==k){
+                cnt++;
+            }
+            if(map.find(sum-k)!=map.end()){
+                cnt+=map[sum-k];
+            }
+            if(map.find(sum) == map.end()){
+                map.insert({sum,1});
+            }
+            else{
+                map[sum]++;
+            }
+        }
+        return cnt;
+    }
+};
