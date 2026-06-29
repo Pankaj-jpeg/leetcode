614a804320de6ab6e23dafa193b7eb55e85713ae
Pankaj Sharma <pankajs8727@gmail.com>
2026-06-29 05:31:03 +0530

    solve(easy): Valid Parentheses [cpp] time=N/A

--- easy/0020-valid-parentheses.cpp
+++ easy/0020-valid-parentheses.cpp
@@ -6,32 +6,29 @@
 // Approach: stack-based matching | Time: O(n) | Space: O(n)
 // Time       : 
 // Space      : 
-// Runtime    : 0 ms  |  Memory: 9 MB
-// Date       : 2026-01-09
+// Runtime    : 0 ms  |  Memory: 8.8 MB
+// Date       : 2026-06-28
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 class Solution {
 public:
     bool isValid(string s) {
-        int i=0; map<char,char> mp = {{'{','}'},{'(',')'},{'[',']'}};
-        stack<char> stk;
+        int i = 0;stack<char> stk;
         while(s[i]!='\0'){
-            if(s[i]=='{' || s[i]=='(' || s[i]=='['){
+            if(s[i] == '(' || s[i] == '[' || s[i] == '{'){
                 stk.push(s[i]);
+                i++;
             }
             else{
-                if(stk.empty()){return false;}
-                else{
-                    char temp = stk.top();
-                    if(mp[temp] == s[i] && !(stk.empty())){
-                        stk.pop();
-                    }
-                    else{return false;}
-                }
+                if(stk.empty()) return false;
+                if((s[i] == ')' && stk.top() != '(') || (s[i] == ']' && stk.top() != '[') || (s[i] == '}' && stk.top() != '{'))      
+                    return false;
+                else
+                    stk.pop();
+                i++;
             }
-            i++;
         }
-        if(!(stk.empty())) {return false;}
-        return true;
+
+        return stk.empty();
     }
 };
