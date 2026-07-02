c8105da685e33d2e7090bcfd2fc8a64c14af0243
Pankaj Sharma <pankajs8727@gmail.com>
2026-07-03 05:26:36 +0530

    solve(hard): Largest Rectangle in Histogram [cpp] time=N/A

--- hard/0084-largest-rectangle-in-histogram.cpp
+++ hard/0084-largest-rectangle-in-histogram.cpp
@@ -3,51 +3,36 @@
 // Difficulty : Hard
 // Link       : https://leetcode.com/problems/largest-rectangle-in-histogram/
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// Approach: two passes, first from right to left to find R[i] and then from left to right to find L[i] | Time: O(n) | Space: O(n)
+// Approach: stack-based histogram scanning | Time: O(n) | Space: O(n)
 // Time       : 
 // Space      : 
-// Runtime    : 11 ms  |  Memory: 88.9 MB
-// Date       : 2026-07-01
+// Runtime    : 3 ms  |  Memory: 78.6 MB
+// Date       : 2026-07-02
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
         int n = heights.size();
-        vector<int> L(n,0);
-        vector<int> R(n,0);
         stack<int,vector<int>> stk;
-
-        for(int i = n-1;i>=0;i--){
-            while(!stk.empty() && heights[i] < heights[stk.top()])
-                stk.pop();
-            
-            if(!stk.empty())
-                R[i] = stk.top() - i - 1;
-            else    
-                R[i] = n- i -1;
-            stk.push(i);
-        }
-
-        while(!stk.empty())
-            stk.pop();
-        
-        for(int i = 0;i<n;i++){
-            while(!stk.empty() && heights[i] <= heights[stk.top()])
-                stk.pop();
-            
-            if(!stk.empty())
-                L[i] = i - stk.top() - 1;
-            else
-                L[i] = i;
-            
-            stk.push(i);
-        }
-
-        
         int Max = 0;
-        for(int i =0 ;i<n;i++){
-            Max = max(Max,heights[i]*(L[i]+R[i]+1));
+
+        for(int i = 0;i<=n;i++){
+            int h;
+            if(i == n)
+                h = 0;
+            else
+                h = heights[i]; 
+            
+            while(!stk.empty() && h <= heights[stk.top()]){
+                int height = heights[stk.top()];
+                stk.pop();
+                int width = stk.empty() ? i : i - stk.top() - 1;
+                Max = max(Max,width*height);
+            }
+
+            stk.push(i);
+
         }
 
         return Max;
