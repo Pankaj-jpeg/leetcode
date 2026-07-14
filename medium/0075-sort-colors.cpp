53dc1d1a3e9066785f58c92c717989f4998326d1
Pankaj Sharma <pankajs8727@gmail.com>
2026-07-15 05:13:58 +0530

    solve(medium): Sort Colors [cpp] time=N/A

--- medium/0075-sort-colors.cpp
+++ medium/0075-sort-colors.cpp
@@ -6,26 +6,33 @@
 // Approach: Dutch National Flag algorithm | Time: O(n) | Space: O(1)
 // Time       : 
 // Space      : 
-// Runtime    : 0 ms  |  Memory: 11.5 MB
-// Date       : 2026-05-24
+// Runtime    : 0 ms  |  Memory: 11.8 MB
+// Date       : 2026-07-14
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int low=0,mid=0,high=nums.size()-1;
-        while(mid<=high){
-            if(nums[mid]==0){
-                swap(nums[low],nums[mid]);
-                low++;mid++;
+        int n = nums.size();
+        int a = 0;
+        int b = n-1;
+        int i = 0;
+
+        while(i<=b){
+            if(i < a){
+                i = a;
+                continue;
             }
-            else if(nums[mid]==1){
-                mid++;
+            if(nums[i] == 0){
+                swap(nums[a++],nums[i]);
+            }
+            else if(nums[i] == 2){
+                swap(nums[b--],nums[i]);
             }
             else{
-                swap(nums[mid],nums[high]);
-                high--;
+                i++;
             }
         }
+        
     }
 };
