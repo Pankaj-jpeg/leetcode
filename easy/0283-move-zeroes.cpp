cc3ed803fef1beb10f281f537fea0f389596b8bc
Pankaj Sharma <pankajs8727@gmail.com>
2026-07-15 05:14:08 +0530

    solve(easy): Move Zeroes [cpp] time=N/A

--- easy/0283-move-zeroes.cpp
+++ easy/0283-move-zeroes.cpp
@@ -3,25 +3,31 @@
 // Difficulty : Easy
 // Link       : https://leetcode.com/problems/move-zeroes/
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// Approach: two-pointer technique, one for non-zero elements and one for zero replacement | Time: O(n) | Space: O(1)
+// Approach: two-pointer technique, one for non-zero elements and one for zero fill | Time: O(n) | Space: O(1)
 // Time       : 
 // Space      : 
-// Runtime    : 0 ms  |  Memory: 23.8 MB
-// Date       : 2026-05-24
+// Runtime    : 0 ms  |  Memory: 23.9 MB
+// Date       : 2026-07-14
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
+        int n = nums.size();
+
         int i = 0;
-        for(int j=0;j<nums.size();j++)
-        {
-            if(nums[j]!=0){
-                nums[i] = nums[j];
-                i++;
+        int j = 0;
+
+        while(j<n){
+            if(nums[j] == 0)
+                j++;
+            else{
+                nums[i++] = nums[j];
+                j++;
             }
         }
-        while(i<nums.size()){
+
+        while(i<n){
             nums[i] = 0;
             i++;
         }
