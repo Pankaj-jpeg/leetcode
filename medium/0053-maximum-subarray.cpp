5dbc51bdf9f30b1249c68d7d52c315801df874a9
Pankaj Sharma <pankajs8727@gmail.com>
2026-07-15 05:13:56 +0530

    solve(medium): Maximum Subarray [cpp] time=N/A

--- medium/0053-maximum-subarray.cpp
+++ medium/0053-maximum-subarray.cpp
@@ -6,24 +6,21 @@
 // Approach: Kadane's algorithm | Time: O(n) | Space: O(1)
 // Time       : 
 // Space      : 
-// Runtime    : 0 ms  |  Memory: 71.8 MB
-// Date       : 2026-05-25
+// Runtime    : 0 ms  |  Memory: 71.7 MB
+// Date       : 2026-07-14
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int max = INT_MIN;
+        int Max = INT_MIN;
         int sum = 0;
-        for(int i=0;i<nums.size();i++){
+        for(int i = 0;i<nums.size();i++){
             sum+=nums[i];
-            if(sum >= max){
-                max = sum;
-            }
-            if(sum < 0){
+            Max = max(Max,sum);
+            if(sum<0)
                 sum = 0;
-            }
         }
-        return max;
+        return Max;
     }
 };
