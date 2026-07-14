1ef85f27c201a16ab0f02d208b1928ba1dd79723
Pankaj Sharma <pankajs8727@gmail.com>
2026-07-15 05:14:06 +0530

    solve(easy): Max Consecutive Ones [cpp] time=N/A

--- easy/0485-max-consecutive-ones.cpp
+++ easy/0485-max-consecutive-ones.cpp
@@ -6,27 +6,26 @@
 // Approach: single pass, reset counter on zero | Time: O(n) | Space: O(1)
 // Time       : 
 // Space      : 
-// Runtime    : 4 ms  |  Memory: 50.1 MB
-// Date       : 2026-05-24
+// Runtime    : 0 ms  |  Memory: 50.1 MB
+// Date       : 2026-07-14
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 class Solution {
 public:
     int findMaxConsecutiveOnes(vector<int>& nums) {
-        int n=0;int temp=0;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i] == 1){
-                temp++;
-            }
-            if(nums[i]==0){
-                if(temp>n)
-                    n = temp;
-                temp = 0;
+        int cnt = 0;
+        int Max = 0;
+        for(int i = 0;i<nums.size();i++){
+            if(nums[i] == 1)
+                cnt++;
+            else{
+                Max = max(Max,cnt);
+                cnt = 0;
             }
         }
-        if(temp > n){
-                n = temp;
-        }
-        return n;
+
+        Max = max(Max,cnt);
+
+        return Max;
     }
 };
