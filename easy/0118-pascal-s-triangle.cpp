d41119521d280d6ed7e9e00e1104cc102cdf4f56
Pankaj Sharma <pankajs8727@gmail.com>
2026-07-15 05:13:44 +0530

    solve(easy): Pascal's Triangle [cpp] time=N/A

--- easy/0118-pascal-s-triangle.cpp
+++ easy/0118-pascal-s-triangle.cpp
@@ -6,27 +6,28 @@
 // Approach: dynamic programming | Time: O(n^2) | Space: O(n^2))
 // Time       : 
 // Space      : 
-// Runtime    : 0 ms  |  Memory: 9.7 MB
-// Date       : 2026-05-26
+// Runtime    : 2 ms  |  Memory: 9.8 MB
+// Date       : 2026-07-14
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 class Solution {
 public:
-    
     vector<vector<int>> generate(int numRows) {
-        vector<vector<int>> res(numRows);
-
-        for (int i = 0; i < numRows; i++) {
-           
-            res[i].resize(i + 1);
-            
-            res[i][0] = res[i][i] = 1;
-            
-            for (int j = 1; j < i; j++) {
-                res[i][j] = res[i-1][j-1] + res[i-1][j];
-            }
-        }
+        vector<vector<int>> res;
+        res.push_back({1});
         
+        for(int i = 1;i<numRows;i++){
+            vector<int> curr;
+            for(int j = 0;j<i+1;j++){
+                if(j == 0 || j == i)
+                    curr.push_back(1);
+                else{
+                    curr.push_back(res[i-1][j-1] + res[i-1][j]);
+                }
+            }
+            res.push_back(curr);
+        }
+
         return res;
     }
 };
