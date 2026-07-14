39952d274e4a55f43c9b31a5a86f7fa2487cc9b9
Pankaj Sharma <pankajs8727@gmail.com>
2026-07-15 05:13:49 +0530

    solve(medium): Longest Consecutive Sequence [cpp] time=N/A

--- medium/0128-longest-consecutive-sequence.cpp
+++ medium/0128-longest-consecutive-sequence.cpp
@@ -3,34 +3,37 @@
 // Difficulty : Medium
 // Link       : https://leetcode.com/problems/longest-consecutive-sequence/
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// Approach: hash set, iterate through each number and check for consecutive sequence | Time: O(n) | Space: O(n)
+// Approach: hash set, check for consecutive sequence | Time: O(n) | Space: O(n)
 // Time       : 
 // Space      : 
-// Runtime    : 83 ms  |  Memory: 88.9 MB
-// Date       : 2026-05-25
+// Runtime    : 75 ms  |  Memory: 89 MB
+// Date       : 2026-07-14
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        unordered_set<int> map;
-        int max=0;int cnt;
-        for(int i =0;i<nums.size();i++){
-            map.insert({nums[i]});
+        unordered_set<int> set;
+        for(int i = 0;i<nums.size();i++){
+            set.insert({nums[i]});
         }
-
-        for(auto num : map){
-            if(map.find(num-1)==map.end()){
-                cnt=1;
-                auto x=num;
-                while(map.find(x+1)!=map.end()){
-                    cnt++;
-                    x++;
+        int curr = 0;
+        int Max = 0;
+        for(auto it : set){
+            if(set.find(it-1) == set.end()){
+                curr = 1;
+                int curr_val = it + 1;
+                while(set.find(curr_val)!=set.end()){
+                    curr++;
+                    curr_val++;
                 }
-                if(cnt>max)
-                    max=cnt;
+                Max = max(Max,curr);
             }
         }
-        return max;
+
+        
+
+        return Max;
+        
     }
 };
