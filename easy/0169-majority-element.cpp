d1f55749d9782c4894169123b34709b78168099c
Pankaj Sharma <pankajs8727@gmail.com>
2026-07-15 05:13:53 +0530

    solve(easy): Majority Element [cpp] time=N/A

--- easy/0169-majority-element.cpp
+++ easy/0169-majority-element.cpp
@@ -6,25 +6,28 @@
 // Approach: Boyer-Moore Voting Algorithm | Time: O(n) | Space: O(1)
 // Time       : 
 // Space      : 
-// Runtime    : 0 ms  |  Memory: 28.2 MB
-// Date       : 2026-05-24
+// Runtime    : 0 ms  |  Memory: 28.3 MB
+// Date       : 2026-07-14
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int count=0,ele;
-
-        for(int i=0;i<nums.size();i++){
-            if(count==0){
-                count++;
-                ele=nums[i];
+        int ele = nums[0];
+        int k = 0;
+        for(int i = 0;i<nums.size();i++){
+            if(nums[i] == ele)
+                k++;
+            else{
+                k--;
             }
-            else if(ele!=nums[i])
-                count--;
-            else
-                count++;
+
+            if(k<0){
+                k = 0;
+                ele = nums[i];
+            }
         }
+
         return ele;
     }
 };
