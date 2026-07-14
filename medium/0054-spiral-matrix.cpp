4e52e54a2d0d50e9f27b38f25c8f339234554895
Pankaj Sharma <pankajs8727@gmail.com>
2026-07-15 05:13:46 +0530

    solve(medium): Spiral Matrix [cpp] time=N/A

--- medium/0054-spiral-matrix.cpp
+++ medium/0054-spiral-matrix.cpp
@@ -3,34 +3,49 @@
 // Difficulty : Medium
 // Link       : https://leetcode.com/problems/spiral-matrix/
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// 
+// Approach: iterative, boundary tracking | Time: O(m*n) | Space: O(m*n)
 // Time       : 
 // Space      : 
 // Runtime    : 0 ms  |  Memory: 9.4 MB
-// Date       : 2026-05-26
+// Date       : 2026-07-14
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        vector<int> result;
-        int m = matrix.size(),n = matrix[0].size();
-        int cnt = m*n;int k=0;
-        while(cnt > result.size()){
-            for(int i=k;i<n-k && cnt > result.size();i++){
-                result.push_back(matrix[k][i]);
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int up ,down ,right, left;
+        up = -1;
+        down = m;
+        right = n;
+        left = -1;
+
+        vector<int> sp_order;
+
+        while(sp_order.size()<m*n){
+            for(int i = left+1;i<right;i++){
+                sp_order.push_back(matrix[up+1][i]);
             }
-            for(int i=k+1;i<m-k && cnt > result.size();i++){
-                result.push_back(matrix[i][n-1-k]);
+            up++;
+            for(int i = up+1;i<down;i++){
+                sp_order.push_back(matrix[i][right-1]);
             }
-            for(int i=n-2-k;i>=k && cnt > result.size();i--){
-                result.push_back(matrix[m-1-k][i]);
+            right--;
+            for(int i = right-1;i>left;i--){
+                sp_order.push_back(matrix[down-1][i]);
             }
-            for(int i=m-2-k;i>=k+1 && cnt > result.size();i--){
-                result.push_back(matrix[i][k]);
+            down--;
+            for(int i = down-1;i>up;i--){
+                sp_order.push_back(matrix[i][left+1]);
             }
-            k++;
+            left++;
         }
-        return result;
+        sp_order.resize(m*n);
+
+        return sp_order;
+
+
+
     }
 };
