d8537a0cf3712d48d62168e24340b2e0f2280412
Pankaj Sharma <pankajs8727@gmail.com>
2026-07-15 05:13:37 +0530

    solve(easy): Find Missing and Repeated Values [cpp] time=N/A

--- easy/2965-find-missing-and-repeated-values.cpp
+++ easy/2965-find-missing-and-repeated-values.cpp
@@ -3,30 +3,40 @@
 // Difficulty : Easy
 // Link       : https://leetcode.com/problems/find-missing-and-repeated-values/
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// Approach: bitwise XOR | Time: O(n^2) | Space: O(n^2))
+// Approach: mathematical derivation of missing and repeated values | Time: O(n^2) | Space: O(1)
 // Time       : 
 // Space      : 
-// Runtime    : 0 ms  |  Memory: 26.8 MB
-// Date       : 2026-06-25
+// Runtime    : 4 ms  |  Memory: 25.7 MB
+// Date       : 2026-07-14
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 class Solution {
 public:
-    vector<int> findMissingAndRepeatedValues(vector<vector<int>>& nums) {
-        int repeating;int n = nums.size();int k = 0;int x = 0;int itr = 1;
-        vector<int> hash(n*n,0);
+    vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
+        long long sq_sum = 0;
+        long long actual_sq_sum = 0;
+        int sum = 0;
+        int actual_sum = 0;
+        int n = grid.size();
+        int itr = 1;
         for(int i = 0;i<n;i++){
-            for(int j =0;j<n;j++){
-                x^=nums[i][j];
-                k^=itr;itr++;
-                if(hash[nums[i][j]-1] == 1)
-                    repeating = nums[i][j];
-                else
-                    hash[nums[i][j]-1] =1;
+            for(int j = 0;j<n;j++){
+                actual_sum += itr;
+                sum+=(grid[i][j]);
+                actual_sq_sum += (itr)*(itr);
+                sq_sum += (grid[i][j])*grid[i][j];
+                itr++;
             }
         }
-        x^=repeating;
-        int missing = k ^ x;;
-        return {repeating,missing};
+        int a;
+        int b;
+
+        int sub = sum - actual_sum;
+        int add = (sq_sum - actual_sq_sum)/sub;
+
+        a = (sub+add)/2;
+        b = add - a;
+        
+        return {a,b};
     }
 };
