cf9a01b7720f9c37771898e9f3a4095ecd26e62d
Pankaj Sharma <pankajs8727@gmail.com>
2026-07-16 05:21:33 +0530

    solve(medium): Single Element in a Sorted Array [cpp] time=N/A

--- medium/0540-single-element-in-a-sorted-array.cpp
+++ medium/0540-single-element-in-a-sorted-array.cpp
@@ -3,35 +3,47 @@
 // Difficulty : Medium
 // Link       : https://leetcode.com/problems/single-element-in-a-sorted-array/
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// Approach: binary search with duplicate handling | Time: O(log n) | Space: O(1)
+// Approach: binary search with adjustment of search space | Time: O(log n) | Space: O(1)
 // Time       : 
 // Space      : 
 // Runtime    : 0 ms  |  Memory: 26.2 MB
-// Date       : 2026-06-26
+// Date       : 2026-07-15
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 class Solution {
 public:
+    
     int singleNonDuplicate(vector<int>& nums) {
-        int low = 0;
-        int high = nums.size()-1;
-        int mid;
         int n = nums.size();
-        
+        int low = 0;
+        int high = n-1;
+        int mid;
+
         while(low<high){
             mid = low + (high-low)/2;
+
+            if(mid == 0 || mid == n-1)
+                break;
+
+
             if(nums[mid] == nums[mid+1]){
-                if((mid-low)%2 != 0)
+                if((mid-low+1)%2 == 0){
                     high = mid-1;
-                else
+                }
+                else{
                     low = mid;
+                }
             }
-            else
-                if((mid-low)%2 != 0)
+
+            else{
+                if((mid-low+1)%2 == 0){
                     low = mid+1;
+                }
                 else
                     high = mid;
+            }
         }
+
         return nums[low];
     }
 };
