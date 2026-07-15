92b26046190d62781e5595298c8b859fc78c7565
Pankaj Sharma <pankajs8727@gmail.com>
2026-07-16 05:21:28 +0530

    solve(easy): Sqrt(x) [cpp] time=N/A

--- easy/0069-sqrt-x.cpp
+++ easy/0069-sqrt-x.cpp
@@ -3,38 +3,38 @@
 // Difficulty : Easy
 // Link       : https://leetcode.com/problems/sqrtx/
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// Approach: binary search | Time: O(log n) | Space: O(1)
+// Approach: binary search | Time: O(log(n)) | Space: O(1)
 // Time       : 
 // Space      : 
-// Runtime    : 0 ms  |  Memory: 8.5 MB
-// Date       : 2026-06-26
+// Runtime    : 0 ms  |  Memory: 8.6 MB
+// Date       : 2026-07-15
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 class Solution {
 public:
     int mySqrt(int x) {
-        long low = 1;
-        long high = x/2 + 1 ;
-        long mid;
-        
-        if(x==0) 
+        int low = 1;
+        int high = x/2;
+        int mid ;
+
+        if(x == 0)
             return 0;
-        if(x==1 || x==2 || x==3)
-            return 1;
+
         while(low<high){
             mid = low + (high-low)/2;
-            if(mid*mid < x)
-                low = mid+1;
-            else
+
+            if((long long)mid*mid >= x)
                 high = mid;
-        }
-        if(low*low == x)
-            return low;
-        else{
-            if(low*low < x)
-                return low+1;
             else
-                return low-1;
+                low = mid+1;
         }
+
+        if((long long)low*low == x)
+            return low;
+
+        if((long long)low*low > x)
+            return low-1;
+        else
+            return low;
     }
 };
