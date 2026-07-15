2d602650498c4cc0e53ea3f8343b28dd4a71f6c8
Pankaj Sharma <pankajs8727@gmail.com>
2026-07-16 05:21:24 +0530

    solve(hard): Split Array Largest Sum [cpp] time=N/A

--- hard/0410-split-array-largest-sum.cpp
+++ hard/0410-split-array-largest-sum.cpp
@@ -6,39 +6,42 @@
 // Approach: binary search | Time: O(n) | Space: O(n)
 // Time       : 
 // Space      : 
-// Runtime    : 0 ms  |  Memory: 10.6 MB
-// Date       : 2026-06-11
+// Runtime    : 0 ms  |  Memory: 10.4 MB
+// Date       : 2026-07-15
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 class Solution {
 public:
-    bool feasible(int mid,vector<int>& nums,int k){
-        int sum =0;int cnt=1;
-        for(int i=0;i<nums.size();i++){
+    bool feasible(vector<int>& nums,int mid, int k){
+        int n = nums.size();
+        int arrs = 1;
+        int sum = 0;
+        for(int i = 0;i<n;i++){
             sum+=nums[i];
             if(sum>mid){
+                arrs++;
                 sum = nums[i];
-                cnt++;
-                if(cnt>k) return false;
             }
         }
-        return true;
+
+        return arrs<=k;
     }
     int splitArray(vector<int>& nums, int k) {
+        int n = nums.size();
         int low = *max_element(nums.begin(),nums.end());
         int high = 0;
-        for(int i=0;i<nums.size();i++){
-            high+=nums[i];
-        }
+        for(auto it : nums)
+            high+=it;
+
         int mid;
         while(low<high){
             mid = low + (high-low)/2;
-            if(feasible(mid,nums,k))
+            if(feasible(nums,mid,k))
                 high = mid;
             else
                 low = mid+1;
         }
-        return low;
 
+        return low;
     }
 };
