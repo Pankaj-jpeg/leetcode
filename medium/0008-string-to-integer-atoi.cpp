866ba4db309e9897960fbf1dd87d21eda18d1de9
Pankaj Sharma <pankajs8727@gmail.com>
2026-07-17 05:20:52 +0530

    solve(medium): String to Integer (atoi) [cpp] time=N/A

--- medium/0008-string-to-integer-atoi.cpp
+++ medium/0008-string-to-integer-atoi.cpp
@@ -3,54 +3,54 @@
 // Difficulty : Medium
 // Link       : https://leetcode.com/problems/string-to-integer-atoi/
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// Approach: recursive depth-first search | Time: O(n) | Space: O(n)
+// Approach: parsing string from left to right, handling sign and leading zeros | Time: O(n) | Space: O(1)
 // Time       : 
 // Space      : 
-// Runtime    : 2 ms  |  Memory: 10.2 MB
-// Date       : 2026-06-17
+// Runtime    : 7 ms  |  Memory: 9.1 MB
+// Date       : 2026-07-16
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 class Solution {
 public:
-    // Helper function that carries the "running total" down the stack
-    int convertRecursive(string s, int i, long currentTotal, int sign) {
-        // Base Case 1: End of string reached
-        if (i >= s.length()) {
-            return currentTotal * sign;
-        }
-        
-        // Base Case 2: Hit a non-digit character (Stop immediately!)
-        if (s[i] < '0' || s[i] > '9') {
-            return currentTotal * sign;
-        }
-        
-        // Progress: Update the running total with the current digit
-        currentTotal = currentTotal * 10 + (s[i] - '0');
-        
-        // Overflow Check: Catch it early on the way down
-        if (sign == 1 && currentTotal > INT_MAX) return INT_MAX;
-        if (sign == -1 && -currentTotal < INT_MIN) return INT_MIN;
-        
-        // Recursive Call: Move to the next index with the updated total
-        return convertRecursive(s, i + 1, currentTotal, sign);
-    }
-
     int myAtoi(string s) {
-        int i = 0;
+        int n = s.size();
         int sign = 1;
         
-        // 1. Skip whitespaces
-        while (i < s.length() && s[i] == ' ') {
+
+        int i = 0;
+        while(s[i] == ' ')
+            i++;
+        
+        if(s[i] =='-'){
+            sign = -1;
             i++;
         }
-        
-        // 2. Handle sign
-        if (i < s.length() && (s[i] == '+' || s[i] == '-')) {
-            sign = (s[i] == '-') ? -1 : 1;
+        else if(s[i] == '+'){
             i++;
         }
+
+        while(s[i] == '0')
+            i++;
         
-        // 3. Kick off the recursion starting at index 'i', with an initial total of 0
-        return convertRecursive(s, i, 0, sign);
+
+        //cout << i << " " << n;
+
+        int res = 0;
+        
+        while((s[i] - '0') <= 9 && (s[i] - '0') >= 0){
+            if((s[i]-'0') + (long long)res*10 <= INT_MAX){
+                res = (s[i]-'0') + (long long)res*10;
+                i++;
+            }
+            else{
+                if(sign == -1)
+                    return INT_MIN;
+                else
+                    return INT_MAX;
+            }
+        }
+        cout << res;
+        return res*sign;
+
     }
 };
