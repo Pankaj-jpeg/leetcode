2657d2a101540b33f5a8f847aca646cc03e077e5
Pankaj Sharma <pankajs8727@gmail.com>
2026-07-19 22:50:31 +0530

    solve(medium): Word Search [cpp] time=N/A

--- medium/0079-word-search.cpp
+++ medium/0079-word-search.cpp
@@ -6,47 +6,43 @@
 // Approach: depth-first search with backtracking | Time: O(m*n*4^l) | Space: O(l))
 // Time       : 
 // Space      : 
-// Runtime    : 241 ms  |  Memory: 10.6 MB
-// Date       : 2026-06-21
+// Runtime    : 191 ms  |  Memory: 10.4 MB
+// Date       : 2026-07-17
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 class Solution {
 public:
-    bool recursive(vector<vector<char>>& board,const string& word,int row,int col,int idx){
-        if(idx == word.length()){
+    bool recur(vector<vector<char>>& board,string& word,int r,int c,int idx){
+        if(idx == word.size())
             return true;
-        }
-        if( row < 0 || row >=board.size() || col < 0 || col >= board[0].size() || board[row][col] != word[idx] || board[row][col] == '#' ) return false;
         
-        char temp = board[row][col];
-        board[row][col]='#';
-
-        //up
-        bool found = recursive(board,word,row-1,col,idx+1) ||
-        //down
-        recursive(board,word,row+1,col,idx+1) ||
-        //right
-        recursive(board,word,row,col+1,idx+1) ||
-        //left
-        recursive(board,word,row,col-1,idx+1);
-
-        board[row][col]=temp;
+        if(r<0 || c<0 || r>=board.size() || c>=board[0].size() || board[r][c]!=word[idx])
+            return false;
         
+        char temp = board[r][c];
+        board[r][c] = '#';
+        bool found = recur(board,word,r-1,c,idx+1) || //up
+        recur(board,word,r+1,c,idx+1) || //down
+        recur(board,word,r,c-1,idx+1) || //left
+        recur(board,word,r,c+1,idx+1);  //right
+
+        board[r][c] = temp;
+
         return found;
-    }   
-
+    }
     bool exist(vector<vector<char>>& board, string word) {
         int m = board.size();
         int n = board[0].size();
 
-        if(m*n < word.length()) return false;
+        if(m*n < word.size())
+            return false;
 
-        for(int r = 0;r<m;r++){
-            for(int c = 0;c<n;c++){
-                if(recursive(board,word,r,c,0)) return true; 
+        for(int i = 0;i<m;i++){
+            for(int j = 0;j<n;j++){
+                if(recur(board,word,i,j,0)) return true;
             }
         }
-        
+
         return false;
     }
 };
