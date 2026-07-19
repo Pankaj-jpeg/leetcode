996a02feb52b79d3117fdf7fc526b16b9b6265e2
Pankaj Sharma <pankajs8727@gmail.com>
2026-07-19 22:50:10 +0530

    solve(medium): Pow(x, n) [cpp] time=N/A

--- medium/0050-pow-x-n.cpp
+++ medium/0050-pow-x-n.cpp
@@ -3,30 +3,28 @@
 // Difficulty : Medium
 // Link       : https://leetcode.com/problems/powx-n/
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// Approach: recursive exponentiation | Time: O(log n) | Space: O(log n)
+// Approach: divide and conquer, using exponentiation by squaring | Time: O(log(n)) | Space: O(1)
 // Time       : 
 // Space      : 
-// Runtime    : 0 ms  |  Memory: 8.8 MB
-// Date       : 2026-06-17
+// Runtime    : 0 ms  |  Memory: 8.6 MB
+// Date       : 2026-07-19
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 class Solution {
 public:
     double myPow(double x, int n) {
-        if(x==1 || n==0)
-            return 1;
-        if(n < 0){
-            if(n == INT_MIN)
-                return 1/x*myPow(x,n+1);
-            return 1/myPow(x,(-1)*n);
+        if(n<0){
+            x = 1/x;
         }
-        if(n==1)
-            return x;
-        
-        if(n%2 == 0)
-            return (myPow(x*x,n/2));
-        else
-            return (x*myPow(x*x,(n-1)/2));
-    }
+        long long nums = labs(n);
+        double Pow = 1;
+        while(nums!=0){
+            if((nums&1))Pow *= x;
 
+            x*=x;
+            nums=(nums>>1);
+        }
+
+        return Pow;
+    }
 };
