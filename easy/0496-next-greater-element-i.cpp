662a0ab6b12c206bfbe3759ddd8e7a73ee5e4440
Pankaj Sharma <pankajs8727@gmail.com>
2026-07-22 05:19:29 +0530

    solve(easy): Next Greater Element I [cpp] time=N/A

--- easy/0496-next-greater-element-i.cpp
+++ easy/0496-next-greater-element-i.cpp
@@ -3,33 +3,30 @@
 // Difficulty : Easy
 // Link       : https://leetcode.com/problems/next-greater-element-i/
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// Approach: stack-based next greater element search | Time: O(n + m) | Space: O(n + m)
+// Approach: stack, pop smaller elements from right to left | Time: O(n) | Space: O(n)
 // Time       : 
 // Space      : 
-// Runtime    : 1 ms  |  Memory: 12.9 MB
-// Date       : 2026-06-30
+// Runtime    : 0 ms  |  Memory: 12.7 MB
+// Date       : 2026-07-21
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        stack<int> stk;unordered_map <int,int> m;
+        stack<int> stk;
+        unordered_map<int,int> m;
         for(int i = nums2.size()-1;i>=0;i--){
-            while(!stk.empty() && nums2[i] > stk.top()){
+            while(!stk.empty() && stk.top() < nums2[i])
                 stk.pop();
-            }
-            if(stk.empty())
-                m.insert({nums2[i],-1});
-            else
-                m.insert({nums2[i],stk.top()});
             
+
+            int larger = stk.empty() ? -1 : stk.top();
+            m.insert({nums2[i],larger});
             stk.push(nums2[i]);
         }
-
-        for(int i =0;i<nums1.size();i++){
-           nums1[i] = m.find(nums1[i])->second;
-        }
-
+        for(int i = 0;i<nums1.size();i++)
+            nums1[i] = m[nums1[i]];
+        
         return nums1;
     }
 };
