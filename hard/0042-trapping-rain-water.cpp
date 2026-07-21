2ca83acc3b0be9168c7a4c02566e9b8365eea4bc
Pankaj Sharma <pankajs8727@gmail.com>
2026-07-22 05:19:27 +0530

    solve(hard): Trapping Rain Water [cpp] time=N/A

--- hard/0042-trapping-rain-water.cpp
+++ hard/0042-trapping-rain-water.cpp
@@ -3,11 +3,11 @@
 // Difficulty : Hard
 // Link       : https://leetcode.com/problems/trapping-rain-water/
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// Approach: two-pointer technique, maintaining max left and right heights | Time: O(n) | Space: O(1)
+// Approach: two-pointer technique, comparing left and right max heights | Time: O(n) | Space: O(1)
 // Time       : 
 // Space      : 
-// Runtime    : 0 ms  |  Memory: 26 MB
-// Date       : 2026-07-01
+// Runtime    : 0 ms  |  Memory: 25.9 MB
+// Date       : 2026-07-21
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 class Solution {
@@ -16,23 +16,25 @@ public:
         int n = height.size();
         int leftMax = 0;
         int rightMax = 0;
-        int total = 0;
         int start = 0;
         int end = n-1;
-        while(start<end){
+        int total = 0;
+
+        while(start < end){
             leftMax = max(leftMax,height[start]);
             rightMax = max(rightMax,height[end]);
 
+
             if(leftMax < rightMax){
-                total += leftMax - height[start];
+                total+=(leftMax - height[start]);
                 start++;
             }
             else{
-                total += rightMax - height[end];
+                total += (rightMax - height[end]);
                 end--;
             }
         }
-        
+
         return total;
  
     }
