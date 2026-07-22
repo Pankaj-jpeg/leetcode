0f56e85ab604493e20c4054a8c48727a764b6722
Pankaj Sharma <pankajs8727@gmail.com>
2026-07-23 05:26:30 +0530

    solve(medium): Longest Substring Without Repeating Characters [cpp] time=N/A

--- medium/0003-longest-substring-without-repeating-characters.cpp
+++ medium/0003-longest-substring-without-repeating-characters.cpp
@@ -3,42 +3,40 @@
 // Difficulty : Medium
 // Link       : https://leetcode.com/problems/longest-substring-without-repeating-characters/
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// Approach: sliding window, update left pointer on duplicate | Time: O(n) | Space: O(n)
+// Approach: sliding window with two pointers, expand right on new char and shrink left on duplicate | Time: O(n) | Space: O(n)
 // Time       : 
 // Space      : 
-// Runtime    : 3 ms  |  Memory: 11.5 MB
-// Date       : 2026-07-05
+// Runtime    : 0 ms  |  Memory: 11.6 MB
+// Date       : 2026-07-22
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         int n = s.size();
-        int i =0;
-        int j =0;
-        vector<int> m(256,-1);
         int Max = 0;
-        while(j<n){
-            if(i == j){
-                m[s[j]] = j;
+        vector<int> freq(256,0);
+        int i = 0;
+        int j = 0;
+
+
+        while(i<n){
+            if(freq[s[i]] == 0){
+                freq[s[i]]++;
+                Max = max(Max,i-j+1);
             }
             else{
-                if(m[s[j]] == -1){
-                    m[s[j]] = j;
+                while(s[j] != s[i]){
+                    freq[s[j]]--;
+                    j++;
                 }
-                else{
-                    if(m[s[j]] >= i){
-                        i=m[s[j]]+1;
-                        m[s[j]] = j;
-                    }
-                    else{
-                        m[s[j]] = j;
-                    }
-                }
+                j++;
             }
-            Max = max(Max,j-i+1);
-            j++;
+            Max = max(Max,i-j+1);
+            i++;
         }
+
         return Max;
+        
     }
 };
