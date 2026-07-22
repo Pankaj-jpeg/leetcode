1865b2e84593bacb38f9565b832d7fa084acf980
Pankaj Sharma <pankajs8727@gmail.com>
2026-07-23 05:26:40 +0530

    solve(hard): Maximal Rectangle [cpp] time=N/A

--- hard/0085-maximal-rectangle.cpp
+++ hard/0085-maximal-rectangle.cpp
@@ -3,47 +3,46 @@
 // Difficulty : Hard
 // Link       : https://leetcode.com/problems/maximal-rectangle/
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// Approach: histogram, stack-based area calculation | Time: O(n*m) | Space: O(n)
+// Approach: histogram, stack-based area calculation | Time: O(m*n) | Space: O(n)
 // Time       : 
 // Space      : 
-// Runtime    : 1 ms  |  Memory: 17.4 MB
-// Date       : 2026-07-02
+// Runtime    : 4 ms  |  Memory: 17.3 MB
+// Date       : 2026-07-22
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 class Solution {
 public:
     int maximalRectangle(vector<vector<char>>& matrix) {
-        vector<int>heights(matrix[0].size(),0);
-        stack<int,vector<int>> stk;
+        int m = matrix.size();
+        int n = matrix[0].size();
         int Max = 0;
-        for(int i = 0;i<matrix.size();i++){
-            int h;
-            for(int j = 0;j<=matrix[0].size();j++){
-                if(j == matrix[0].size())
-                    h = 0;
-                else{
-                    if((matrix[i][j]-'0') == 0)
-                        heights[j] = 0;
-                    else
-                        heights[j]++;
-                    
-                    h = heights[j];
-                }
+        vector<int> heights(n,0);
+        stack<int> stk;
+        for(int i = 0;i<m;i++){
+            for(int j = 0;j<n;j++){
+                if(matrix[i][j] == '1')
+                    heights[j]++;
+                else 
+                    heights[j] = 0;
+            }
 
-                while(!stk.empty() && heights[stk.top()] >= h){
-                    int height = heights[stk.top()];
+
+            for(int j = 0;j<=n;j++){
+                while(!stk.empty() && (j == n || heights[stk.top()] >= heights[j])){
+                    int h = heights[stk.top()];
                     stk.pop();
                     int width = stk.empty() ? j : j - stk.top() - 1;
-                    Max = max(Max,height*width);
+                    Max = max(Max,h*width);
                 }
-
                 stk.push(j);
-
             }
 
             while(!stk.empty())
                 stk.pop();
+            
         }
+
+
         return Max;
     }
 };
